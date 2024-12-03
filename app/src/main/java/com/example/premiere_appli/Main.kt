785e47b0d06b566fe8bf8fded4ae60a0a661c89d
Clement Lorieau <lrc4559a@etu.iut-tlse3.fr>
2024-12-03 14:46:16 +0100

    exercice 2 ok

--- app/src/main/java/com/example/premiere_appli/Main.kt
+++ app/src/main/java/com/example/premiere_appli/Main.kt
@@ -11,11 +11,24 @@ class Cuisine : Piece(longueur = 1.5, largeur = 2.0, nom = "cuisine" )
 
 class Salon : Piece(longueur = 15.0, largeur = 18.5, nom = "salon" )
 
+class Etudiant (val name : String, val promo : String, val matieres : List<String>)
+
+val etudiants = listOf(
+    Etudiant ("Paul", "2025", listOf("mobile", "web", "BDD")),
+    Etudiant ("Yazid", "2024", listOf("mobile", "Android", "Réseau")),
+    Etudiant ("Caroline", "2025", listOf("SE", "Anglais")),
+)
+
+
+
 fun main (items: List<Cuisine>, elements: List<Salon>) {
     for (item in items) println("La ${item.nom} mesure ${item.surface()} m²")
     for (element in elements) println("Le ${element.nom} mesure ${element.surface()} m²")
+    etudiants.filter {it.promo == "2024"}.forEach{ println(it.name)}
+    etudiants.filter { it.matieres.size > 2 }.forEach { println(it.name) }
 
 }
+
 
 
 
